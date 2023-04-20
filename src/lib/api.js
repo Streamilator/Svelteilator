795501f28048132205f68467dbd3b963bf19d201
M.D. Walters <mail@mdwalters.ml>
2023-04-20 16:42:44 +0000

    chore: add `package-lock.json`

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,7 @@
 /**
 * Gets a list of songs
-* @returns {Promise<object[]>} A list of songs
 */
 export async function get_songs() {
-    const songs = await fetch("https://sapi.streamilator.tk/songs.json").then(res => res.json());
+    const songs = await fetch("https://streamilator-sapi.pages.dev/songs.json").then(res => res.json());
     return songs.songs;
 }
