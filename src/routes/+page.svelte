67fda7f1c4e7f410a52378e289d8b271ee195c6a
M.D. Walters <mail@mdwalters.ml>
2023-04-26 05:13:29 +0000

    chore: port to tailwind (sort of) solved my problems

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,12 @@
     <title>Home - Svelteilator</title>
 </svelte:head>
 
-<div class="container grid">
+<div class="flex space-x-2 overflow-y-auto h-[200px] m-3">
 	{#await get_songs()}
         Loading...
-    {:then songs} 
+    {:then songs}
         {#each songs as song}
-           <div class="song">
+           <div class="song w-auto h-auto p-[30px]">
                 <b>{song.name}</b>
                 <br>
                 <small>{song.author}</small>
@@ -20,4 +20,11 @@
     {:catch error}
         <code>{error}</code>
     {/await}
+</div>
+
+<div class="fixed bottom-0 w-full h-[120px] p-[30px] justify-center text-center" style="background-color: rgb(29, 29, 29);">
+    <b>Your mom</b>
+    <small>Someone's mom</small>
+    <input type="range" min="1" max="100" value="1" class="w-full h-[15px] slider" id="slider">
+    play
 </div>
