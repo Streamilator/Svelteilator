dfd46d6032b4433f1d31cface476fc2e2ff09422
M.D. Walters <mail@mdwalters.ml>
2023-04-26 12:02:47 +0000

    feat: redesign!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,23 @@
 <script>
     import { get_songs } from "$lib/api.js";
+    import { Icon, Play } from "svelte-hero-icons";
 </script>
 
 <svelte:head>
     <title>Home - Svelteilator</title>
 </svelte:head>
 
-<div class="flex space-x-2 overflow-y-auto h-[200px] m-3">
+<div class="flex flex-col space-y-2 m-3">
 	{#await get_songs()}
         Loading...
     {:then songs}
         {#each songs as song}
-           <div class="song w-auto h-auto p-[30px]">
-                <b>{song.name}</b>
+           <div class="song w-auto h-fill p-[30px]" on:click={() => {
+                alert("no");
+            }}>
+                <b class="text-2xl">{song.name}</b>
                 <br>
-                <small>{song.author}</small>
+                <small class="text-lg">{song.author}</small>
            </div> 
         {/each}
     {:catch error}
@@ -22,9 +25,10 @@
     {/await}
 </div>
 
+<br><br><br><br><br>
+
 <div class="fixed bottom-0 w-full h-[120px] p-[30px] justify-center text-center" style="background-color: rgb(29, 29, 29);">
-    <b>Your mom</b>
-    <small>Someone's mom</small>
+    <b>nothing is playing</b>
     <input type="range" min="1" max="100" value="1" class="w-full h-[15px] slider" id="slider">
-    play
+    <Icon src={Play} solid class="w-[30px] h-[30px] text-center" />
 </div>
