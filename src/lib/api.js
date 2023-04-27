bd2174c9ecfb5ee632664ed278d905bc9577de1b
M.D. Walters <mail@mdwalters.ml>
2023-04-27 19:55:41 +0000

    fix: use the proper api url

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@
 * Gets a list of songs
 */
 export async function get_songs() {
-    const songs = await fetch("https://streamilator-sapi.pages.dev/songs.json").then(res => res.json());
+    const songs = await fetch("https://semi-api.streamilator-01z.pages.dev/songs.json").then(res => res.json());
     return songs.songs;
 }
 
@@ -11,6 +11,6 @@ export async function get_songs() {
 * @param {number} id The song ID to play
 */
 export function play(id) {
-    const audio = new Audio(`https://streamilator-sapi.pages.dev/songs/${id}.mp3`);
+    const audio = new Audio(`https://semi-api.streamilator-01z.pages.dev/songs/${id}.mp3`);
     audio.play();
 }
